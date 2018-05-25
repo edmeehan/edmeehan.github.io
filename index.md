604f306b0e758bd9eff1f7ea2ffcdf3fdc99ba9f
Edward Meehan <edward.meehan.808@gmail.com>
2018-05-25 00:45:20 -0700

    new template updated

--- index.md
+++ index.md
@@ -1,5 +1,5 @@
 ---
-layout: default
+layout: altitude_index
 title: Welcome
 headline: Edward Meehan
 blockName: me
--- js/main.js
+++ js/main.js
@@ -1,78 +1,353 @@
-/******/ (function(modules) { // webpackBootstrap
-/******/ 	// The module cache
-/******/ 	var installedModules = {};
-/******/
-/******/ 	// The require function
-/******/ 	function __webpack_require__(moduleId) {
-/******/
-/******/ 		// Check if module is in cache
-/******/ 		if(installedModules[moduleId]) {
-/******/ 			return installedModules[moduleId].exports;
-/******/ 		}
-/******/ 		// Create a new module (and put it into the cache)
-/******/ 		var module = installedModules[moduleId] = {
-/******/ 			i: moduleId,
-/******/ 			l: false,
-/******/ 			exports: {}
-/******/ 		};
-/******/
-/******/ 		// Execute the module function
-/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
-/******/
-/******/ 		// Flag the module as loaded
-/******/ 		module.l = true;
-/******/
-/******/ 		// Return the exports of the module
-/******/ 		return module.exports;
-/******/ 	}
-/******/
-/******/
-/******/ 	// expose the modules object (__webpack_modules__)
-/******/ 	__webpack_require__.m = modules;
-/******/
-/******/ 	// expose the module cache
-/******/ 	__webpack_require__.c = installedModules;
-/******/
-/******/ 	// define getter function for harmony exports
-/******/ 	__webpack_require__.d = function(exports, name, getter) {
-/******/ 		if(!__webpack_require__.o(exports, name)) {
-/******/ 			Object.defineProperty(exports, name, {
-/******/ 				configurable: false,
-/******/ 				enumerable: true,
-/******/ 				get: getter
-/******/ 			});
-/******/ 		}
-/******/ 	};
-/******/
-/******/ 	// getDefaultExport function for compatibility with non-harmony modules
-/******/ 	__webpack_require__.n = function(module) {
-/******/ 		var getter = module && module.__esModule ?
-/******/ 			function getDefault() { return module['default']; } :
-/******/ 			function getModuleExports() { return module; };
-/******/ 		__webpack_require__.d(getter, 'a', getter);
-/******/ 		return getter;
-/******/ 	};
-/******/
-/******/ 	// Object.prototype.hasOwnProperty.call
-/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
-/******/
-/******/ 	// __webpack_public_path__
-/******/ 	__webpack_require__.p = "";
-/******/
-/******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 0);
-/******/ })
-/************************************************************************/
-/******/ ([
-/* 0 */
-/***/ (function(module, exports, __webpack_require__) {
+/*
+	Altitude by Pixelarity
+	pixelarity.com | hello@pixelarity.com
+	License: pixelarity.com/license
+*/
 
-"use strict";
+var settings = {
 
+	slider: {
 
-document.addEventListener("DOMContentLoaded", function (event) {
-    document.querySelector('html').classList.add('play');
-});
+		// Transition speed (in ms)
+		// For timing purposes only. It *must* match the transition speed of ".slider > article".
+			speed: 1500,
 
-/***/ })
-/******/ ]);
+		// Transition delay (in ms)
+			delay: 4000
+
+	},
+
+	carousel: {
+
+		// Transition speed (in ms)
+		// For timing purposes only. It *must* match the transition speed of ".carousel > article".
+			speed: 350
+
+	}
+
+};
+
+(function($) {
+
+	skel.breakpoints({
+		xlarge: '(max-width: 1680px)',
+		large: '(max-width: 1280px)',
+		medium: '(max-width: 980px)',
+		small: '(max-width: 736px)',
+		xsmall: '(max-width: 480px)',
+		xxsmall: '(max-width: 360px)'
+	});
+
+	/**
+	 * Custom slider for Altitude.
+	 * @return {jQuery} jQuery object.
+	 */
+	$.fn._slider = function(options) {
+
+		var	$window = $(window),
+			$this = $(this);
+
+		// Handle no/multiple elements.
+			if (this.length == 0)
+				return $this;
+
+			if (this.length > 1) {
+
+				for (var i=0; i < this.length; i++)
+					$(this[i])._slider(options);
+
+				return $this;
+
+			}
+
+		// Vars.
+			var	current = 0, pos = 0, lastPos = 0,
+				slides = [],
+				$slides = $this.children('article'),
+				intervalId,
+				isLocked = false,
+				i = 0;
+
+		// Functions.
+			$this._switchTo = function(x, stop) {
+
+				// Handle lock.
+					if (isLocked || pos == x)
+						return;
+
+					isLocked = true;
+
+				// Stop?
+					if (stop)
+						window.clearInterval(intervalId);
+
+				// Update positions.
+					lastPos = pos;
+					pos = x;
+
+				// Hide last slide.
+					slides[lastPos].removeClass('top');
+
+				// Show new slide.
+					slides[pos].addClass('visible').addClass('top');
+
+				// Finish hiding last slide after a short delay.
+					window.setTimeout(function() {
+
+						slides[lastPos].addClass('instant').removeClass('visible');
+
+						window.setTimeout(function() {
+
+							slides[lastPos].removeClass('instant');
+							isLocked = false;
+
+						}, 100);
+
+					}, options.speed);
+
+			};
+
+		// Slides.
+			$slides
+				.each(function() {
+
+					var $slide = $(this);
+
+					// Add to slides.
+						slides.push($slide);
+
+					i++;
+
+				});
+
+		// Initial slide.
+			slides[pos]
+				.addClass('visible')
+				.addClass('top');
+
+		// Bail if we only have a single slide.
+			if (slides.length == 1)
+				return;
+
+		// Main loop.
+			intervalId = window.setInterval(function() {
+
+				// Increment.
+					current++;
+
+					if (current >= slides.length)
+						current = 0;
+
+				// Switch.
+					$this._switchTo(current);
+
+			}, options.delay);
+
+	};
+
+	/**
+	 * Custom carousel for Altitude.
+	 * @return {jQuery} jQuery object.
+	 */
+	$.fn._carousel = function(options) {
+
+		var	$window = $(window),
+			$this = $(this);
+
+		// Handle no/multiple elements.
+			if (this.length == 0)
+				return $this;
+
+			if (this.length > 1) {
+
+				for (var i=0; i < this.length; i++)
+					$(this[i])._slider(options);
+
+				return $this;
+
+			}
+
+		// Vars.
+			var	current = 0, pos = 0, lastPos = 0,
+				slides = [],
+				$slides = $this.children('article'),
+				intervalId,
+				isLocked = false,
+				i = 0;
+
+		// Functions.
+			$this._switchTo = function(x, stop) {
+
+				// Handle lock.
+					if (isLocked || pos == x)
+						return;
+
+					isLocked = true;
+
+				// Stop?
+					if (stop)
+						window.clearInterval(intervalId);
+
+				// Update positions.
+					lastPos = pos;
+					pos = x;
+
+				// Hide last slide.
+					slides[lastPos].removeClass('visible');
+
+				// Finish hiding last slide after a short delay.
+					window.setTimeout(function() {
+
+						// Hide last slide (display).
+							slides[lastPos].hide();
+
+						// Show new slide (display).
+							slides[pos].show();
+
+						// Show new new slide.
+							window.setTimeout(function() {
+								slides[pos].addClass('visible');
+							}, 25);
+
+						// Unlock after sort delay.
+							window.setTimeout(function() {
+								isLocked = false;
+							}, options.speed);
+
+					}, options.speed);
+
+			};
+
+		// Slides.
+			$slides
+				.each(function() {
+
+					var $slide = $(this);
+
+					// Add to slides.
+						slides.push($slide);
+
+					// Hide.
+						$slide.hide();
+
+					i++;
+
+				});
+
+		// Nav.
+			$this
+				.on('click', '.next', function(event) {
+
+					// Prevent default.
+						event.preventDefault();
+						event.stopPropagation();
+
+					// Increment.
+						current++;
+
+						if (current >= slides.length)
+							current = 0;
+
+					// Switch.
+						$this._switchTo(current);
+
+				})
+				.on('click', '.previous', function(event) {
+
+					// Prevent default.
+						event.preventDefault();
+						event.stopPropagation();
+
+					// Decrement.
+						current--;
+
+						if (current < 0)
+							current = slides.length - 1;
+
+					// Switch.
+						$this._switchTo(current);
+
+				});
+
+		// Initial slide.
+			slides[pos]
+				.show()
+				.addClass('visible');
+
+		// Bail if we only have a single slide.
+			if (slides.length == 1)
+				return;
+
+	};
+
+	$(function() {
+
+		var	$window = $(window),
+			$body = $('body');
+
+		// Disable animations/transitions until the page has loaded.
+			$body.addClass('is-loading');
+
+			$window.on('load', function() {
+				window.setTimeout(function() {
+					$body.removeClass('is-loading');
+				}, 100);
+			});
+
+		// Fix: Placeholder polyfill.
+			$('form').placeholder();
+
+		// Prioritize "important" elements on medium.
+			skel.on('+medium -medium', function() {
+				$.prioritize(
+					'.important\\28 medium\\29',
+					skel.breakpoint('medium').active
+				);
+			});
+
+		// Fix: Object-fit (pseudo) polyfill.
+			if (!skel.canUse('object-fit'))
+				$('img[data-position]').each(function() {
+
+					var	$this = $(this),
+						$parent = $this.parent();
+
+					// Apply img's src to parent.
+						$parent
+							.css('background-image', 'url("' + $this.attr('src') + '")')
+							.css('background-size', 'cover')
+							.css('background-repeat', 'no-repeat')
+							.css('background-position', $this.data('position'));
+
+					// Hide img.
+						$this
+							.css('opacity', '0');
+
+				});
+
+		// Sliders.
+			$('.slider')
+				._slider(settings.slider);
+
+		// Carousels.
+			$('.carousel')
+				._carousel(settings.carousel);
+
+		// Menu.
+			$('#menu')
+				.append('<a href="#menu" class="close"></a>')
+				.appendTo($body)
+				.panel({
+					delay: 500,
+					hideOnClick: true,
+					hideOnSwipe: true,
+					resetScroll: true,
+					resetForms: true,
+					target: $body,
+					visibleClass: 'is-menu-visible',
+					side: 'right'
+				});
+
+	});
+
+})(jQuery);
